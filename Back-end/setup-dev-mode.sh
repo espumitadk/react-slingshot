06a92f4c088ff8bb3923d3d1ca0b00c83c7f557c
espumitadk <davidfalconnaranjo@gmail.com>
2017-06-24 20:34:48 +0100

    Change backend tecnology

--- Back-end/setup-dev-mode.sh
+++ Back-end/setup-dev-mode.sh
@@ -5,4 +5,4 @@ docker stop back-cont
 docker rm back-cont
 docker rmi back-img
 docker build -t back-img $DOCKERFILE_DIRECTORY
-docker run --name back-cont -p 8080:8080 -d back-img
+docker run --name back-cont -p 80:4567 -d back-img
