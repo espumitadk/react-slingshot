b6e5e142ebf2b3c22352ea2952ac701631dc6c15
espumitadk <davidfalconnaranjo@gmail.com>
2017-07-08 10:27:52 +0100

    create controller

---
+++ Back-end/Controllers/StateController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace app.Controllers {
+
+    [Route("[controller]")]
+    public class StateController : Controller {
+     
+        [HttpPost]
+        public string playerMovement([FromBody]Example value) {
+            return value.name;
+        }
+    }
+
+    public class Example {
+        public string name { get;  set;}
+    }
+}
