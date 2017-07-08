e8a1e09b41477575cb5065d44a15114eca8298fd
espumitadk <davidfalconnaranjo@gmail.com>
2017-07-08 10:27:09 +0100

    enable CORS

--- Back-end/Startup.cs
+++ Back-end/Startup.cs
@@ -8,12 +8,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace app
-{
-    public class Startup
-    {
-        public Startup(IHostingEnvironment env)
-        {
+namespace app {
+
+    public class Startup {
+
+        public Startup(IHostingEnvironment env) {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
@@ -24,19 +23,27 @@ namespace app
 
         public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        // This method gets callhttps://docs.microsoft.com/en-us/aspnet/core/security/corsed by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
             // Add framework services.
+            services.AddCors(options => {
+                options.AddPolicy("MY-CORS-POLICY",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials() );
+            });
             services.AddMvc();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-
+                
+            app.UseCors("MY-CORS-POLICY");
+            
             app.UseMvc();
         }
     }
