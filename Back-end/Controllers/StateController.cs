7556c6972da15a6d253e3cc7cd49e3a945551f2d
espumitadk <davidfalconnaranjo@gmail.com>
2017-07-08 19:34:58 +0100

    change state in backend

--- Back-end/Controllers/StateController.cs
+++ Back-end/Controllers/StateController.cs
@@ -10,7 +10,7 @@ namespace app.Controllers {
     public class StateController : Controller {
      
         [HttpPost]
-        public IActionResult playerMovement([FromBody]GameState state) {
+        public IActionResult playerMovement([FromBody]IEnumerable<Cell> state) {
             var serverMovement = new Movement();
             var RandomColumn = new System.Random().Next(1, 8);
             serverMovement.column = "column" + RandomColumn;
@@ -18,23 +18,10 @@ namespace app.Controllers {
         }
     }
 
-    public class GameState {
-        public Row row1 { get;  set;}
-        public Row row2 { get;  set;}
-        public Row row3 { get;  set;}
-        public Row row4 { get;  set;}
-        public Row row5 { get;  set;}
-        public Row row6 { get;  set;}
-    }
-
-    public class Row {
-        public string column1 { get;  set;}
-        public string column2 { get;  set;}
-        public string column3 { get;  set;}
-        public string column4 { get;  set;}
-        public string column5 { get;  set;}
-        public string column6 { get;  set;}
-        public string column7 { get;  set;}
+    public class Cell {
+        public string row { get;  set;}
+        public string column { get;  set;}
+        public string player { get;  set;}
     }
 
     public class Movement {
