74954c0f1fbec71108d33156515785dc894933b1
espumitadk <davidfalconnaranjo@gmail.com>
2017-07-08 09:20:08 +0100

    make backend technology great again

--- Back-end/setup-dev-mode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DOCKERFILE_DIRECTORY=~/IdeaProjects/react-slingshot/Back-end
-docker stop back-cont
-docker rm back-cont
-docker rmi back-img
-docker build -t back-img $DOCKERFILE_DIRECTORY
-docker run --name back-cont -p 80:4567 -d back-img
