eaf754d1060b76fab6ecc0ebbeb3054935f3f9bb
espumitadk <davidfalconnaranjo@gmail.com>
2017-07-15 12:00:35 +0100

    modify demon api

--- artificial-ntelligence/src/run.py
+++ artificial-ntelligence/src/run.py
@@ -4,32 +4,19 @@ from game.Heuristic import heuristic
 
 player = 'X'
 problem_pLayer = player
-
 game = ConnectFour()
 state = game.initial
 
-while True:
-    print "Jugador a mover:", game.to_move(state)
-    game.display(state)
+for key, value in { (1, 1): 'X', (1, 2): 'X', (1, 3): 'X', (2, 1): 'X' }.iteritems():
+    state.board.setdefault(key, value)
+    state.moves.remove(key)
 
-    if player == 'O':
-        col_str = raw_input("Movimiento: ")
-        coor = int(str(col_str).strip())
-        x = coor
-        y = -1
-        legal_moves = game.legal_moves(state)
-        for lm in legal_moves:
-            if lm[0] == x:
-                y = lm[1]
-        state = game.make_move((x, y), state)
-        player = 'X'
-    else:
-        print "Thinking..."
-        move = games.alphabeta_search(state, game, 4, None, heuristic, problem_pLayer)
-        state = game.make_move(move, state)
-        player = 'O'
+if game.terminal_test(state):
+    print "Final de la partida"
+else:
+    print "Thinking..."
+    move = games.alphabeta_search(state, game, 4, None, heuristic, problem_pLayer)
+    state = game.make_move(move, state)
     print "-------------------"
-    if game.terminal_test(state):
-        game.display(state)
-        print "Final de la partida"
-        break
+
+game.display(state)
