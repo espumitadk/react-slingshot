89abb042f931743f1eb9dff0586123ab1c44cb75
espumitadk <davidfalconnaranjo@gmail.com>
2017-07-15 19:20:32 +0100

    Error: try to use socket connection

--- Back-end/Controllers/StateController.cs
+++ Back-end/Controllers/StateController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 
 namespace app.Controllers {
 
@@ -14,8 +17,12 @@ namespace app.Controllers {
             var serverMovement = new Movement();
             var RandomColumn = new System.Random().Next(1, 8);
             serverMovement.column = "column" + RandomColumn;
+            TcpClient client = new TcpClient();
+            client.Client.Connect(IPAddress.Parse("artificial-intelligence"), 5051);
+            client.Client.Send(Encoding.UTF8.GetBytes("[{\"x\": 1,\"y\": 1,\"player\": \"X\"},{\"x\": 1,\"y\": 2,\"player\": \"X\"},{\"x\": 1,\"y\": 3,\"player\": \"X\"}]"));           
             return Json(serverMovement);
         }
+
     }
 
     public class Cell {
@@ -27,5 +34,7 @@ namespace app.Controllers {
     public class Movement {
         public string column { get;  set;}
     }
+
+    
 
 }
