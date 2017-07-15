79890eb6aeb3ac7c53c0accebe800277540cf3df
espumitadk <davidfalconnaranjo@gmail.com>
2017-07-15 13:19:30 +0100

    set manual daemon

--- artificial-ntelligence/src/run.py
+++ artificial-ntelligence/src/run.py
@@ -2,15 +2,12 @@ from game import games
 from game.ConnectFour import ConnectFour
 from game.Heuristic import heuristic
 
-# https://pypi.python.org/pypi/python-daemon/#downloads
-import daemon
+game = ConnectFour()
+state = game.initial
+for key, value in { (1, 1): 'X', (1, 2): 'X', (1, 3): 'X', (2, 1): 'X' }.iteritems():
+    state.board.setdefault(key, value)
+    state.moves.remove(key)
+print games.alphabeta_search(state, game, 4, None, heuristic, 'X')
 
-from spam import do_main_program
+raw_input()
 
-with daemon.DaemonContext():
-    game = ConnectFour()
-    state = game.initial
-    for key, value in { (1, 1): 'X', (1, 2): 'X', (1, 3): 'X', (2, 1): 'X' }.iteritems():
-        state.board.setdefault(key, value)
-        state.moves.remove(key)
-    print games.alphabeta_search(state, game, 4, None, heuristic, 'X')
