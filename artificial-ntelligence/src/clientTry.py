458f7d346c5a28edf7c9120a7c4337e548985124
espumitadk <davidfalconnaranjo@gmail.com>
2017-07-15 16:59:16 +0100

    set artificial-intelligence socket comunication

---
+++ artificial-ntelligence/src/clientTry.py
@@ -0,0 +1,14 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    request = """[{"x": 1,"y": 1,"player": "X"},{"x": 1,"y": 2,"player": "X"},{"x": 1,"y": 3,"player": "X"}]"""
+    server.connect(("localhost", 5051))
+    server.send(request.encode())
+    result = server.recv(1024)
+    print result
+except Exception as e:
+    print e
+    server.close()
+    exit(-1)
+server.close()
--- artificial-ntelligence/src/run.py
+++ artificial-ntelligence/src/run.py
@@ -1,13 +1,32 @@
 from game import games
 from game.ConnectFour import ConnectFour
 from game.Heuristic import heuristic
+import socket
+import json
 
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind(("localhost", 5051))
+serverSocket.listen(1)
 game = ConnectFour()
-state = game.initial
-for key, value in { (1, 1): 'X', (1, 2): 'X', (1, 3): 'X', (2, 1): 'X' }.iteritems():
-    state.board.setdefault(key, value)
-    state.moves.remove(key)
-print games.alphabeta_search(state, game, 4, None, heuristic, 'X')
 
-raw_input()
-
+while True:
+    connection, address = serverSocket.accept()
+    try:
+        print 'Connection address:', address
+        state = game.initial
+        while 1:
+            data = connection.recv(1024)
+            if not data: break
+            else:
+                newState = json.loads(data)
+                print "received cells:", len(newState)
+                for cell in newState:
+                    state.board.setdefault((cell["x"], cell["y"]), cell["player"])
+                    state.moves.remove((cell["x"], cell["y"]))
+                move = games.alphabeta_search(state, game, 4, None, heuristic, 'X')
+                connection.send("{ movement: '" + str(move[0]) + "'}")
+        connection.close()
+    except Exception as e:
+        print e
+        connection.close()
+        exit(-1)
