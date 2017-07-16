ce604a30b6fd04e68cc40635de6951573f5d0749
espumitadk <davidfalconnaranjo@gmail.com>
2017-07-16 20:24:35 +0100

    fix socket

--- artificial-intelligence/src/run.py
+++ artificial-intelligence/src/run.py
@@ -7,30 +7,35 @@ import json
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind(("artificial-intelligence", 5051))
 serverSocket.listen(1)
-game = ConnectFour()
-
 while True:
-    connection, address = serverSocket.accept()
+    game = ConnectFour()
+    state = game.initial
     try:
+        connection, address = serverSocket.accept()
         print 'Connection address:', address
-        state = game.initial
+    except Exception as exception:
+        print exception
+        connection.close()
+        serverSocket.close()
+        exit(-1)
+    try:
         while 1:
             data = connection.recv(1024)
             if not data: break
             else:
                 newState = json.loads(data)
-                print "received cells:", len(newState)
+                print "received cells:", newState
                 if newState:
                     for cell in newState:
                         state.board.setdefault((cell["x"], cell["y"]), cell["player"])
                         state.moves.remove((cell["x"], cell["y"]))
                 move = games.alphabeta_search(state, game, 4, None, heuristic, 'X')
-                print(str(move[0]))
-                game.display(state)
                 connection.send(str(move[0]))
-                exit(-2)
+                break
         connection.close()
-    except Exception as e:
-        print e
+
+    except Exception as exception:
+        print exception
         connection.close()
+        serverSocket.close()
         exit(-1)
