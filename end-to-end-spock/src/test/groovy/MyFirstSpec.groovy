399a70bac6466382e41edce489785cd3225f266f
espumitadk <davidfalconnaranjo@gmail.com>
2017-08-02 19:39:08 +0100

    tescontainers-spock helloworld

--- end-to-end-spock/src/test/groovy/MyFirstSpec.groovy
+++ end-to-end-spock/src/test/groovy/MyFirstSpec.groovy
@@ -1,18 +1,27 @@
 import com.groovycoder.spockdockerextension.Testcontainers
+import org.testcontainers.containers.GenericContainer
+import redis.clients.jedis.Jedis
+import spock.lang.Shared
 import spock.lang.Specification
 
 @Testcontainers
 class MyFirstSpec extends Specification{
 
-    def "EXAMPLE TEST"(){
+    @Shared
+    private GenericContainer redisContainer = new GenericContainer("redis:3.0.6")
+                                                         .withExposedPorts(6379)
+
+    def "EXAMPLE REDIS CONTAINER TEST"(){
+        given:
+        Jedis redisClient = new Jedis(redisContainer.getContainerIpAddress(), redisContainer.getMappedPort(6379))
+        redisClient.set("name","DAVID")
+
         when:
-        name.size() == length
+        String value = redisClient.get("name")
 
         then:
-        name        | length
-        "Spock"     |   5
-        "Spoc"      |   4
-        "Spockk"    |   6
-
+        value == "DAVID"
     }
 }
+
+
